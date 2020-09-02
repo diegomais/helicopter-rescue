a6cfe09f4a7e55701ca8a51695ff9d8d64c8411b
Diego Mais <diegomais.dev@gmail.com>
2020-09-02 18:37:22 -0300

    💫 Hide card and append players

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function start() {
+  $("#card").hide();
+  $("#container").append("<div id='apache'></div>");
+  $("#container").append("<div id='chopper'></div>");
+  $("#container").append("<div id='truck'></div>");
+  $("#container").append("<div id='friend'></div>");
+}
