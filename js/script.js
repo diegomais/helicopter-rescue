5ee54087484ff10a4212ba76cedcb03ff28f3c47
Diego Mais <diegomais.dev@gmail.com>
2020-09-02 18:43:11 -0300

    💫 Add players animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function start() {
   $("#card").hide();
-  $("#container").append("<div id='apache'></div>");
-  $("#container").append("<div id='chopper'></div>");
+  $("#container").append("<div id='apache' class='apache-animation'></div>");
+  $("#container").append("<div id='chopper' class='chopper-animation'></div>");
   $("#container").append("<div id='truck'></div>");
-  $("#container").append("<div id='friend'></div>");
+  $("#container").append("<div id='friend' class='friend-animation'></div>");
 }
