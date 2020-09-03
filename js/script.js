b859a36758404106c88303ace60f4582c4aa1f73
Diego Mais <diegomais.dev@gmail.com>
2020-09-02 21:35:21 -0300

    💫 Add movements to apache

--- js/script.js
+++ js/script.js
@@ -6,14 +6,52 @@ function start() {
   $("#container").append("<div id='friend' class='friend-animation'></div>");
 
   var game = {};
+
+  var keycode = { w: 87, s: 83, d: 68 };
+
+  game.keyPressed = {};
+
+  $(document).keydown(function (e) {
+    game.keyPressed[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    game.keyPressed[e.which] = false;
+  });
+
   game.loop = setInterval(loop, 30);
 
   function loop() {
     moveBackground();
+    moveApache();
   }
 
   function moveBackground() {
     var left = parseInt($("#container").css("background-position"));
     $("#container").css("background-position", left - 1);
+  }
+
+  function moveApache() {
+    if (game.keyPressed[keycode.w]) {
+      var top = parseInt($("#apache").css("top"));
+      $("#apache").css("top", top - 10);
+
+      if (top <= 0) {
+        $("#apache").css("top", top + 10);
+      }
+    }
+
+    if (game.keyPressed[keycode.s]) {
+      var top = parseInt($("#apache").css("top"));
+      $("#apache").css("top", top + 10);
+
+      if (top >= 434) {
+        $("#apache").css("top", top - 10);
+      }
+    }
+
+    if (game.keyPressed[keycode.d]) {
+      console.log("d");
+    }
   }
 }
