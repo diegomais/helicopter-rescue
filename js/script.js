a75c81ed38ce00880bb7206503f5c719f172e246
Diego Mais <diegomais.dev@gmail.com>
2020-09-02 21:28:28 -0300

    💫 Animate background

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ function start() {
   $("#container").append("<div id='chopper' class='chopper-animation'></div>");
   $("#container").append("<div id='truck'></div>");
   $("#container").append("<div id='friend' class='friend-animation'></div>");
+
+  var game = {};
+  game.loop = setInterval(loop, 30);
+
+  function loop() {
+    moveBackground();
+  }
+
+  function moveBackground() {
+    var left = parseInt($("#container").css("background-position"));
+    $("#container").css("background-position", left - 1);
+  }
 }
